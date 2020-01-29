1d94ce03f0f9df9c6d5cf98ab1f7747e7c220033
Eric Sink <eric@sourcegear.com>
2020-01-29 13:56:19 -0600

    declutter

--- ray.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-time wasmtime ./target/wasm32-wasi/release/rust-raytracer.rustc.wasm 0
--- rayapp/src/util/mod.rs
+++ rayapp/src/util/mod.rs
@@ -2,12 +2,6 @@ use rand::{RngCore};
 
 pub mod export;
 
-#[cfg(target_os = "wasi")]
-pub fn get_rng() -> Box<dyn RngCore> {
-    Box::new(wasi_rng::WasiRng)
-}
-
-#[cfg(not(target_os = "wasi"))]
 pub fn get_rng() -> Box<dyn RngCore> {
     Box::new(rand::thread_rng())
 }
